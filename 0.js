65d22b60695dde8499fbfa8152926fe656a0d043
zlj520 <774245653@qq.com>
2023-03-26 14:45:38 +0800

    new

--- 0.js
+++ 0.js
@@ -688,7 +688,7 @@ function do_zhuanxiang() {
   // 下面是倒序答题
   if (zhuanxiang_dao) {
     // 当出现已满分时，点击最后一个开始答题
-    while (!text("2018/12/18").exists()) {
+    while (!text("已满分").exists()) {
       scoll.scrollForward();
       // 不加延迟会很卡
       sleep(200);
